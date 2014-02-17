91966f4e6909579e50dfd332f7d1936663c09e16
zaqzaq <382566697@qq.com>
2014-02-17 17:05:33 +0800

    commit 14=02-17

--- other/SQLscript/PULL_SQL.sql
+++ other/SQLscript/PULL_SQL.sql
@@ -1,6 +1,32 @@
 -- 1
 DROP TABLE IF EXISTS `pull_send_message`;
 DROP TABLE IF EXISTS `pull_message`;
+DROP TABLE IF EXISTS `app_user`;
+
+CREATE TABLE `app_user` (
+  `userId` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `staffNo` varchar(128) DEFAULT NULL COMMENT '编号',
+  `username` varchar(128) NOT NULL COMMENT '用户名',
+  `title` smallint(6) NOT NULL COMMENT '1=先生\r\n            0=女士\r\n            小姐',
+  `password` varchar(128) NOT NULL COMMENT '密码',
+  `email` varchar(128) NOT NULL COMMENT '邮件',
+  `depId` bigint(20) DEFAULT NULL COMMENT '所属部门',
+  `position` varchar(32) DEFAULT NULL COMMENT '职位',
+  `phone` varchar(32) DEFAULT NULL COMMENT '电话',
+  `mobile` varchar(32) DEFAULT NULL COMMENT '手机',
+  `faxPhone` varchar(128) DEFAULT NULL COMMENT '传真',
+  `fax` varchar(32) DEFAULT NULL COMMENT '直线',
+  `address` varchar(64) DEFAULT NULL COMMENT '地址',
+  `zip` varchar(32) DEFAULT NULL COMMENT '邮编',
+  `photo` varchar(128) DEFAULT NULL COMMENT '相片地址',
+  `accessionTime` datetime NOT NULL COMMENT '入职时间',
+  `status` smallint(6) NOT NULL COMMENT '状态\r\n            1=激活\r\n            0=禁用\r\n            2=离职\r\n            ',
+  `education` varchar(64) DEFAULT NULL,
+  `fullname` varchar(50) NOT NULL,
+  `delFlag` smallint(6) NOT NULL COMMENT '0=未删除\r\n            1=删除',
+  `departureTime` datetime DEFAULT NULL COMMENT '离职时间'
+) ENGINE=InnoDB AUTO_INCREMENT=248 DEFAULT CHARSET=utf8 COMMENT='app_user\r\n用户表';
+
 
 CREATE TABLE `pull_message` (
   `messageId` bigint(20) NOT NULL AUTO_INCREMENT,
